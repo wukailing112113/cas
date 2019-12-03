dcda3edc93eb6e2641d8aa326b23c2c752b6e7e6
wukl <wukl@massestech.com>
2019-12-03 18:00:11 +0800

    cas 服务端和客户端

---
+++ cas-client2/src/test/java/casclient/casclient2/CasClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package casclient.casclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
